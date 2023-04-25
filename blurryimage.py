7aaa93d4e1d3da1d02e9fb2bf903bf093695d070
Miro Ackermann <uvhab@student.kit.edu>
2023-04-25 10:15:14 +0200

    improved readability of blurryimage.py

--- blurryimage.py
+++ blurryimage.py
@@ -2,12 +2,15 @@ import numpy as np
 import scipy
 from PIL import Image
 
-image = Image.open("heart.png")
+# image to array conversion
+image = Image.open("test.png")
 img_array = np.array(image)
 image.show()
+
+
 # kernel convolution
 
-
+# first idea with horrible runtime
 def kernel_convolution_loop(array, radius=1):
     amount_rows = len(array)
     amount_columns = len(array[0][:])
@@ -31,13 +34,13 @@ def kernel_convolution_loop(array, radius=1):
     return new_array
 
 
+# second idea with long runtime
 def kernel_convolution_slice(array, radius=1):
     amount_rows = len(array)
     amount_columns = len(array[0])
     new_array = np.zeros(dtype="u1", shape=(amount_rows, amount_columns, 3))
     for row in range(0, amount_rows):
         for column in range(0, amount_columns):
-
             buffer_array = np.zeros(shape=3, dtype="i8")
             square = np.asarray(array[np.max([0, row - radius]): np.min([row + radius + 1, amount_rows]),
                                 np.max([0, column - radius]): np.min([column + radius + 1, amount_columns])])
@@ -46,65 +49,35 @@ def kernel_convolution_slice(array, radius=1):
             blue = square[:, :, 2].sum()
             buffer_array[0:3] = [red, green, blue]
 
-            buffer_array = np.divide(buffer_array,   square.size / 3)
+            buffer_array = np.divide(buffer_array, square.size / 3)
 
             new_array[row][column] = buffer_array
 
     return new_array
 
-def kernel_convolution_with_iterator(array, radius=1):
-    amount_rows = len(array)
-    amount_columns = len(array[0])
-    new_array = np.zeros(dtype="u1", shape=(amount_rows, amount_columns, 3))
 
-    for index, x in np.ndenumerate(array):
-        buffer_array = np.zeros(shape=3, dtype="i8")
-        square = np.asarray(array[np.max([0, index[0] - radius]): np.min([index[0] + radius + 1, amount_rows]),
-                            np.max([0, index[1] - radius]): np.min([index[1] + radius + 1, amount_columns])])
-
-        red = square[:, :, 0].sum()
-        green = square[:, :, 1].sum()
-        blue = square[:, :, 2].sum()
-        buffer_array[0:3] = [red, green, blue]
-        buffer_array = np.divide(buffer_array, square.size / 3)
-        new_array[index[0]][index[1]] = buffer_array
-
-    return new_array
-
-
-def kernel_convolution_element(array, radius=1):
-    kernel = np.divide(np.ones((radius + 2, radius + 2, 1)), (radius + 2)**2)
-    new_image = scipy.signal.convolve(array, kernel, mode= 'same')
-    new_image = np.asarray(new_image, dtype='u1')
-    print(kernel.shape)
-    print(new_image.shape)
-    return new_image
-
-def gaussian_convolution_element(array, radius=1):
-    para_a = 2
-    kernel = np.reshape(np.exp(-np.power(np.linspace(-radius / para_a, radius / para_a, num=para_a*radius + 1), 2)), (para_a*radius+1, 1))
-    kernel = kernel @ np.transpose(kernel)
-    kernel = np.reshape(kernel, (para_a*radius+1, para_a*radius+1, 1))
-    print(kernel.shape)
-    print(array.shape)
+# third idea with scipy.signal.convolve
+def kernel_convolution(array, radius=1):
+    kernel = np.divide(np.ones((radius + 2, radius + 2, 1)), (radius + 2) ** 2)
     new_image = scipy.signal.convolve(array, kernel, mode='same')
     new_image = np.asarray(new_image, dtype='u1')
     return new_image
 
-#print(img_array.shape)
-#mean_pixel_value(img_array)
-#kernel_convolution_element(img_array, 2)
-new_img_array = gaussian_convolution_element(img_array,5)
+
+# third idea but with gaussian kernel
+def gaussian_convolution(array, radius: int = 1, a=1.0):
+    kernel = np.reshape(np.exp(-np.power(np.linspace(-radius * a, radius * a, num=2 * radius + 1), 2)),
+                        (2 * radius + 1, 1))
+    kernel = kernel @ np.transpose(kernel)
+
+    kernel = np.reshape(kernel, (2 * radius + 1, 2 * radius + 1, 1))
+    new_image = scipy.signal.convolve(array, kernel / kernel.sum(keepdims=False), mode='same')
+    new_image = np.asarray(new_image, dtype='u1')
+    return new_image
+
+
+# parameter chosen through experimentation
+new_img_array = gaussian_convolution(img_array, 128, 0.032)
 new_image = Image.fromarray(new_img_array)
 new_image.show()
-#new_img_array = kernel_convolution_loop(img_array, 50)
-#new_image = Image.fromarray(new_img_array)
-#new_image.show()
-
-#new_image.save("blurredHeart.png")
-#jessi_img = Image.open("Jessi_und_Molly.jpg")
-#jessi_array = np.asarray(jessi_img)
-#blurred_jessi_img = Image.fromarray(kernel_convolution_slice(jessi_array, radius=80))
-#blurred_jessi_img.show()
-#blurred_jessi_img.save("Jessi_und_Molly_r80.jpg")
-exit()
+# new_image.save("blurred_test.png")
