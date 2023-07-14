34ebc38f24d4db9cecd692cc68a000b3ef123a8e
Orimalc <miro.ackermann@yahoo.com>
2023-07-14 15:56:05 +0200

    I edited the test and game has now the steal mechanic, but I need to test it more and also implement it for P2

--- Kahala.py
+++ Kahala.py
@@ -3,67 +3,77 @@ import numpy as np
 
 class Game:
     def __init__(self):
-        self.board = np.asarray([[4, 4, 4, 4, 4, 4], [4, 4, 4, 4, 4, 4]])
-        self.scores = [0, 0]
+        self.board = np.asarray([4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0])
         self.is_played = True
         self.turn_of_player = 1
 
-    # For Testing
-    def set_board(self, two_x_six_array):
-        self.board = two_x_six_array
-
     def play(self, user_input):
+
+        if user_input > 6 or user_input < 1:
+            return
         distance = user_input
-        column = distance - 1
-        if self.turn_of_player == 2:
-            column = 5 - column
-        amount_of_stones = self.board[self.turn_of_player - 1, column]
-        if amount_of_stones >= distance:
-            self.scores[self.turn_of_player - 1] += 1
-
-        # TODO amount_of_stones - distance > 6
-        for i in range(1, amount_of_stones + 1):
+        if self.turn_of_player == 1:
+            column = 6 - distance
+        else:
+            column = 13 - distance
+        amount_of_stones = self.board[column]
+        self.board[column] = 0
+        if amount_of_stones + 6 - distance > 12:
+            amount_of_stones += 1
             if self.turn_of_player == 1:
-                j = column - i
-
-                if j < - 7:
-                    j = 6 - -j % 6
-                if j < -1:
-                    self.board[self.turn_of_player, -j - 2] += 1
-                elif j > -1:
-                    self.board[self.turn_of_player - 1, j] += 1
+                self.board[13] -= 1
             else:
-                k = column + i
+                self.board[6] -= 1
 
-                if k > 11:
-                    k = k % 6
-                if k > 6:
-                    self.board[self.turn_of_player - 2, -(7 - k)] += 1
-                elif column + i < 6:
-                    self.board[self.turn_of_player - 1, k] += 1
-        self.board[self.turn_of_player - 1, column] = 0
-
+        if column + amount_of_stones > 13:
+            diff = 13 - (column + amount_of_stones)
+            self.board[column + 1: column + amount_of_stones + diff + 1] += 1
+            self.board[0:-diff] += 1
+            self.try_to_steal(distance, column, amount_of_stones, diff)
+        else:
+            self.board[column + 1:column + amount_of_stones + 1] += 1
+            self.try_to_steal(distance, column, amount_of_stones)
         if amount_of_stones != distance:
             self.turn_of_player = self.turn_of_player % 2 + 1
 
-    def startGame(self):
-        while (self.is_played):
+    def start_game(self):
+        while self.is_played:
             print(f"Turn of Player: {self.turn_of_player}")
-            print(f"Score: Player 1:Player 2  ||  {self.scores[0]}:{self.scores[1]}")
+            print(f"Score: Player 1:Player 2  ||  {self.board[6]}:{self.board[13]}")
             print("State of the Board")
             print("Row of Player 1: ["
-                  f"{self.board[0, 0]}][{self.board[0, 1]}]"
-                  f"[{self.board[0, 2]}][{self.board[0, 3]}][{self.board[0, 4]}][{self.board[0, 5]}]")
+                  f"{self.board[5]}][{self.board[4]}]"
+                  f"[{self.board[3]}][{self.board[2]}][{self.board[1]}][{self.board[0]}]")
             print("       <-----     1  2  3  4  5  6")
             print("Row of Player 2: ["
-                  f"{self.board[1, 0]}][{self.board[1, 1]}]"
-                  f"[{self.board[1, 2]}][{self.board[1, 3]}][{self.board[1, 4]}][{self.board[1, 5]}]")
+                  f"{self.board[7]}][{self.board[8]}]"
+                  f"[{self.board[9]}][{self.board[10]}][{self.board[11]}][{self.board[12]}]")
             print("       ----->     6  5  4  3  2  1")
             self.play(int(input()))
+            if self.game_over():
+                self.is_played = False
             print("".ljust(100, "*"))
             print("\n\n\n")
+
+    def game_over(self):
+
+        if np.asarray(self.board[0:6]).all() or np.asarray(self.board[7:13]).all():
+            return True
+
+        return False
+
+    def try_to_steal(self, distance, column, amount_of_stones, diff=0):
+        #TODO implement with -diff > 0
+        if self.turn_of_player == 1 and column + amount_of_stones < 6:
+            if self.board[column + amount_of_stones] == 1 \
+                    and self.board[column + amount_of_stones + (column + 1)] > 0:
+                self.board[6] += self.board[column + amount_of_stones + (column + 1)]
+                self.board[column + amount_of_stones + (column + 1)] = 0
+                self.board[column + amount_of_stones] = 0
+                return
+
 
 
 if __name__ == "__main__":
     game = Game()
-    game.startGame()
+    game.start_game()
--- test_Kahala.py
+++ test_Kahala.py
@@ -3,33 +3,34 @@ from unittest import TestCase
 import numpy as np
 import Kahala
 
+
 class TestGame(TestCase):
 
     def test_fillingUpByTravelingP1(self):
         game1 = Kahala.Game()
-        game1.set_board(np.asarray([[1, 2, 3, 4, 4, 5], [0, 0, 0, 0, 0, 0]]))
+        game1.board = np.asarray([5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
         game1.play(6)
-        self.assertEqual([[2, 3, 4, 5, 5, 0], [0, 0, 0, 0, 0, 0]], game1.board.tolist())
+        self.assertEqual([0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0], game1.board.tolist())
 
     def test_toEnemyRowP1(self):
         game1 = Kahala.Game()
-        game1.set_board(np.asarray([[4, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]]))
+        game1.board = np.asarray([0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0])
         game1.play(1)
-        self.assertEqual([[0, 0, 0, 0, 0, 0], [1, 1, 1, 0, 0, 0]], game1.board.tolist())
+        self.assertEqual([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0], game1.board.tolist())
 
     def test_backToOwnBoardP1(self):
         game1 = Kahala.Game()
-        game1.board = np.asarray([[8,0,0,0,0,0],[0,0,0,0,0,0]])
+        game1.board = np.asarray([0, 0, 0, 0, 0, 8, 0, 0, 0, 0, 0, 0, 0, 0])
         game1.play(1)
-        self.assertEqual([[0,0,0,0,0,1],[1,1,1,1,1,1]], game1.board.tolist())
+        self.assertEqual([1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0], game1.board.tolist())
         game2 = Kahala.Game()
-        game2.board=np.asarray([[0,0,0,0,0,13],[0,0,0,0,0,0]])
+        game2.board = np.asarray([13, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
         game2.play(6)
-        self.assertEqual([[1,1,1,1,1,1],[1,1,1,1,1,1]], game2.board.tolist())
+        self.assertEqual([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0], game2.board.tolist())
         game3 = Kahala.Game()
-        game3.board=np.asarray([[0,0,0,0,14,0],[0,0,0,0,0,0]])
+        game3.board = np.asarray([0, 14, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
         game3.play(5)
-        self.assertEqual([[1,1,1,2,1,1],[1,1,1,1,1,1]], game3.board.tolist())
+        self.assertEqual([1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0], game3.board.tolist())
 
     def test_stealingP1(self):
         game1 = Kahala.Game()
@@ -39,32 +40,31 @@ class TestGame(TestCase):
 
     def test_fillingUpByTravelingP2(self):
         game1 = Kahala.Game()
-        game1.board=np.asarray([[0, 1, 0, 0, 0, 0], [5, 0, 0, 0, 0, 0]])
+        game1.board = np.asarray([0, 0, 0, 0, 1, 0, 0, 5, 0, 0, 0, 0, 0, 0])
         game1.play(2)
         game1.play(6)
-        self.assertEqual([[1, 0, 0, 0, 0, 0], [0, 1, 1, 1, 1, 1]], game1.board.tolist())
+        self.assertEqual([0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0], game1.board.tolist())
 
     def test_toEnemyRowP2(self):
         game1 = Kahala.Game()
-        game1.board=np.asarray([[0, 0, 0, 3, 0, 0], [0, 0, 0, 0, 0, 3]])
+        game1.board = np.asarray([0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0])
         game1.play(4)
         game1.play(1)
-        self.assertEqual([[1, 1, 1, 0, 1, 1], [0, 0, 0, 0, 0, 0]], game1.board.tolist())
+        self.assertEqual([1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1], game1.board.tolist())
 
     def test_backToOwnBoardP2(self):
         game1 = Kahala.Game()
-        game1.board=np.asarray([[0, 0, 1, 0, 0, 0], [0, 0, 0, 0, 0, 8]])
+        game1.board = np.asarray([0, 0, 0, 1, 0, 0,0, 0, 0, 0, 0, 0, 8,0])
         game1.play(3)
         game1.play(1)
-        self.assertEqual([[1, 2, 1, 1, 1, 1], [1, 0, 0, 0, 0, 0]], game1.board.tolist())
+        self.assertEqual([1, 1, 1, 1, 2, 1,0, 1, 0, 0, 0, 0, 0,1], game1.board.tolist())
         game2 = Kahala.Game()
-        game2.board=np.asarray([[0, 0, 1, 0, 0, 0], [13, 0, 0, 0, 0, 0]])
+        game2.board = np.asarray([0, 0, 0, 1, 0, 0,0, 13, 0, 0, 0, 0, 0, 0])
         game2.play(3)
         game2.play(6)
-        self.assertEqual([[1, 2, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1]], game2.board.tolist())
+        self.assertEqual([1, 1, 1, 1, 2, 1,0, 1, 1, 1, 1, 1, 1, 1], game2.board.tolist())
         game3 = Kahala.Game()
-        game3.board=np.asarray([[0, 0, 1, 0, 0, 0], [0, 14, 0, 0, 0, 0]])
-        game3.play(3)
+        game3.board = np.asarray([0, 0, 1, 0, 0, 0, 0, 0, 14, 0, 0, 0, 0,0])
+        game3.play(4)
         game3.play(5)
-        self.assertEqual([[1, 2, 1, 1, 1, 1], [1, 1, 2, 1, 1, 1]], game3.board.tolist())
-
+        self.assertEqual([1, 1, 1, 2, 1, 1,0, 1, 1, 2, 1, 1, 1, 1], game3.board.tolist())
